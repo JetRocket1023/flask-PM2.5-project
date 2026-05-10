89cb629124305109cef285d57a67cc5c3284bfc7
Rock <rockman761023@gmail.com>
2026-05-10 16:07:26 +0800

    完成最新日期數據跟繪製

--- database.py
+++ database.py
@@ -10,6 +10,39 @@ load_dotenv()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
+def get_latest_data():
+    conn, cursor = open_db()
+    result = {"success": True, "message": None, "columns": None, "rows": None}
+    if not conn:
+        result["success"] = False
+        result["massage"] = "資料庫開啟失敗"
+        return result
+
+    sql = """
+    select * from data where datacreationdate=
+    (select max(datacreationdate) from data);
+    """
+
+    # sql = "select max(datacreationdate) from data;"
+    try:
+        cursor.execute(sql)
+        # 取得資料欄位名稱
+        # print(cursor.description)
+        columns = [col[0] for col in cursor.description]
+
+        rows = cursor.fetchall()
+        result["success"] = True
+        result["columns"] = columns
+        result["rows"] = rows
+        return result
+    except Exception as e:
+        result["success"] = False
+        result["massage"] = f"資料庫查詢失敗(e)"
+        return result
+    finally:
+        conn.close()
+
+
 def open_db():
     try:
         # ost=os.getenv.get("HOST") //本地使用
@@ -29,4 +62,4 @@ def open_db():
     return None, None
 
 
-print(open_db())
+print(get_latest_data())
