1a5af148e7d30d30c273dc02cc0c2ce810d4cad4
Rock <rockman761023@gmail.com>
2026-05-17 10:51:27 +0800

    完成查詢用api

--- database.py
+++ database.py
@@ -10,6 +10,58 @@ load_dotenv()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
+def get_data_by_county(county):
+    conn, cursor = open_db()
+    result = {"success": True, "message": None, "rows": None}
+    if not conn:
+        result["success"] = False
+        result["massage"] = "資料庫開啟失敗"
+        return result
+
+    sql = """select * from data where county=%s
+    and datacreationdate=(select max(datacreationdate) from data);"""
+    try:
+        cursor.execute(sql, (county,))
+        # 取得資料欄位名稱
+        # print(cursor.description)
+        rows = cursor.fetchall()
+        result["success"] = True
+        result["rows"] = rows
+        return result
+    except Exception as e:
+        result["success"] = False
+        result["massage"] = f"資料庫查詢失敗{e}"
+        return result
+    finally:
+        conn.close()
+
+
+# 取得不重複的縣市
+def get_counties():
+    conn, cursor = open_db()
+    result = {"success": True, "message": None, "rows": None}
+    if not conn:
+        result["success"] = False
+        result["massage"] = "資料庫開啟失敗"
+        return result
+    sql = "select DISTINCT  county from data ORDER BY county DESC;"
+    try:
+        cursor.execute(sql)
+        # 取得資料欄位名稱
+        # print(cursor.description)
+        rows = cursor.fetchall()
+        result["success"] = True
+        result["rows"] = rows
+        return result
+    except Exception as e:
+        result["success"] = False
+        result["massage"] = f"資料庫查詢失敗{e}"
+        return result
+    finally:
+        conn.close()
+
+
+# 取得最新資料
 def get_latest_data():
     conn, cursor = open_db()
     result = {"success": True, "message": None, "columns": None, "rows": None}
@@ -37,7 +89,7 @@ def get_latest_data():
         return result
     except Exception as e:
         result["success"] = False
-        result["massage"] = f"資料庫查詢失敗(e)"
+        result["massage"] = f"資料庫查詢失敗{e}"
         return result
     finally:
         conn.close()
@@ -62,4 +114,5 @@ def open_db():
     return None, None
 
 
-print(get_latest_data())
+if __name__ == "__main__":
+    print(get_data_by_county("新北市"))
